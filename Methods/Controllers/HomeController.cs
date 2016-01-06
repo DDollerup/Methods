afc8afa297b490b4aca5cd5c5977de9cc8f780b5
PDDaniel <daniel@proper-dog.com>
2016-01-06 12:51:03 +0100

    Save

---
+++ Methods/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Methods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Hine
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string text)
+        {
+            string polishedText = string.Empty;
+            RemoveSpaces(text, out polishedText);
+
+
+
+            ViewBag.Result = polishedText;
+            return View("Index");
+        }
+
+        public int AddNumbers(int x, int y)
+        {
+            return x + y;
+        }
+
+        public void RemoveSpaces(string text, out string result)
+        {
+            string textWithoutSpaces = text.Replace(" ", "");
+
+            // Gør alle bugstaverne til store bugstaver
+            // result = textWithoutSpaces.ToUpper();
+
+            // Gør alle bugstaverne til små bugstaver
+            // result = textWithoutSpaces.ToLower();
+
+             result = textWithoutSpaces;
+        }
+    }
+}
